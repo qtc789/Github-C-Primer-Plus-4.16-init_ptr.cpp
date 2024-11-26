2136744d6dbfff4b6301162208c9a2df5e4c4ab0
qtc789 <qiantongci@126.com>
2024-11-26 21:38:50 +0800

    Add files via upload

---
+++ 4.16/4.16 init_ptr.cpp/4.16 init_ptr.cpp/4.16 init_ptr.cpp.cpp
@@ -0,0 +1,19 @@
+﻿// 4.16 init_ptr.cpp.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	int higgens=5;
+	int* pt = &higgens;
+	cout << "values    " << higgens << endl;
+	cout << "*p_updates" << *pt << endl;
+	cout << &higgens << endl;
+	cout << pt << endl;
+	
+
+
+
+}
